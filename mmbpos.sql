f143b455da4f41be0aeed482a8a8bf1c9f317896
andrewpablo2005 <158246156+andrewpablo2005@users.noreply.github.com>
2026-09-07 10:55:55 +0800

    push the latest database and tables

--- mmbpos.sql
+++ mmbpos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 07, 2026 at 04:50 AM
+-- Generation Time: Sep 07, 2026 at 04:53 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
